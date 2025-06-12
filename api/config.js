b0d1e6b0f235f6e2855e3f2a439403561b07cdf7
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-06-12 04:06:15 +0200

    used arrow function and called the function

--- api/config.js
+++ api/config.js
@@ -1,9 +1,8 @@
-let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
-let apiKey = checkApiKey();
-
-function checkApiKey() {
+const checkApiKey = () => {
   if (!localStorage.getItem("apiKey")) {
     window.open("enter-api-key.html", "_self");
   }
   return localStorage.getItem("apiKey");
-}
+};
+
+checkApiKey();
