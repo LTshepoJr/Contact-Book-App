d41af5024f9f9847237bb9f95bb8225956fbf71b
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-06-13 03:10:18 +0200

    added contacts from api, made refresh and log out buttons work

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+const refresh = document.getElementById("refresh");
+const addContact = document.getElementById("add-contact");
+const logOut = document.getElementById("log-out");
+const contactWrapper = document.getElementById("contact-wrapper");
+
 window.addEventListener("load", () => {
-  //   console.log(rootPath);
-  //   console.log(apiKey);
   fetch(rootPath + "controller/get-contacts/")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       //   displayOutput(data);
-      console.log(data);
+      data.forEach(({ avatar, firstname, email, lastname, mobile, id }) => {
+        const contact = `<div id="contact">
+          <img
+            src="${rootPath}controller/uploads/${avatar}"
+            alt="${firstname} ${lastname} ${id}"
+          />
+          <div>
+            <h2>${firstname} ${lastname}</h2>
+            <p>${email}</p>
+            <p>${mobile}</p>
+          </div>
+        </div>`;
+        contactWrapper.insertAdjacentHTML("beforeend", contact);
+      });
     });
+});
+
+refresh.addEventListener("click", () => {
+  location.reload();
+});
+
+logOut.addEventListener("click", () => {
+  localStorage.removeItem("apiKey");
+  location.reload();
 });
