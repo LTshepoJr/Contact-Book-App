ea397704287c0d81de7033e8af7cae1a44f9f3ae
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-06-15 05:59:16 +0200

    added event listener to button and created new form to append apiKey

--- add-contact.js
+++ add-contact.js
@@ -1,5 +1,16 @@
 const contactBook = document.getElementById("contact-book");
+const addContactSubmit = document.getElementById("add-contact-submit");
 
 contactBook.addEventListener("click", () => {
   window.open("index.html", "_self");
+});
+
+addContactSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const contactForm = document.getElementById("form-contact");
+  const newForm = new FormData(contactForm);
+  newForm.append("apiKey", apiKey);
+  for (const [key, value] of newForm.entries()) {
+    console.log(`${key}:`, value);
+  }
 });
